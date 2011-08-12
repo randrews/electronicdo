f03aeb841cc733d3b70a0b56cb0474a98661f866
Ross Andrews <randrews@geekfu.org>
2011-08-12 11:36:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Electronicdo::Application.config.secret_token = '6c23ed3c0c71589d8931b24038a55ab7cb30c9064b8eceb45ce04eefb36fe31795316aa8bf4fe42366372c397f9215ea1d2ee6efd86c2cf6ee924f9108c50f26'
