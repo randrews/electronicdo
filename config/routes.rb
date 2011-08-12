7ed318c1e82e73093aa6f7d7f9e626f8a497d14d
Ross Andrews <randrews@geekfu.org>
2011-08-12 15:01:51 -0500

    Welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Electronicdo::Application.routes.draw do
+
+    root :to => "welcome#index"
+
     # The priority is based upon order of creation:
     # first created -> highest priority.
 
@@ -45,10 +48,6 @@ Electronicdo::Application.routes.draw do
     #     # (app/controllers/admin/products_controller.rb)
     #     resources :products
     #   end
-
-    # You can have the root of your site routed with "root"
-    # just remember to delete public/index.html.
-    # root :to => "welcome#index"
 
     # See how all your routes lay out with "rake routes"
 
